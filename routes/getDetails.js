2ffdbacdd85b78ff337ea5f8d30eb0ce361ebad0
Digi18 <me.digvijay18@gmail.com>
2019-12-03 11:52:55 +0530

    Chnges

--- routes/getDetails.js
+++ routes/getDetails.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
+const dotEnv = require('dotenv').config();
 
 const dburl = process.env.URL;
 
--- routes/saveDetails.js
+++ routes/saveDetails.js
@@ -2,42 +2,45 @@ const express = require('express');
 const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const dotEnv = require('dotenv').config();
 
 const dburl = process.env.URL;
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended: true}));
 
 router.post('/saveDetails',(req,res) => {
 
       var data = {
 
-        // id:req.body.id,
+         id:req.body.id,
          name:req.body.fullname,
          mobile:req.body.mobile,
          date:req.body.date
        };
 
-    MongoClient.connect(dburl,{useNewUrlParser:true,useUnifiedTopology:true},(err,client) => {
-                 
-                   if(err){
-                       console.log('Error',err);
-                   }
-                   else{
+   MongoClient.connect(dburl,{ useNewUrlParser: true,useUnifiedTopology:true},(err,client) => {
 
-                    var collect = client.db('Easypolicy_db').collection('Policies');
+              if(err){
+                  console.log("Error",err);
+              }
+              else{
+                  
+                let collection = client.db('Easypolicy_db').collection('Policies');
 
-                    collect.insertOne(data, (err,resp) => {
+                collection.insertOne(data,(err,resp) => {
+                          
+                    if(err){
+                        console.log("Db Error",err);
+                    }
+                    else{
 
-                                  if(err){
-                                    console.log('Error',err);
-                                  }
-                                  else{
-
-                                    res.send("Data add successfully");
-                                  }
-
-                    });
-            }
-    });
-
+                        res.send("Data added successfully");
+                        client.close();
+                    }
+                });
+              }
+   });
 });
 
 module.exports = router;
