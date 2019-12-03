2af1d55c8100d8b3795d839b77609d8cb9134bb0
Digi18 <me.digvijay18@gmail.com>
2019-12-03 10:43:16 +0530

    first commit

---
+++ routes/saveDetails.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+
+const dburl = process.env.URL;
+
+router.post('/saveDetails',(req,res) => {
+
+      var data = {
+
+         id:req.body.id,
+         name:req.body.fullname,
+         mobile:req.body.mobile,
+         date:req.body.date
+       };
+
+    MongoClient.connect(dburl,{useNewUrlParser:true,useUnifiedTopology:true},(err,client) => {
+                 
+                   if(err){
+                       console.log('Error',err);
+                   }
+                   else{
+
+                    var collect = client.db('Easypolicy_db').collection('Policies');
+
+                    collect.insertOne(data, (err,resp) => {
+
+                                  if(err){
+                                    console.log('Error',err);
+                                  }
+                                  else{
+
+                                    res.send("Data add successfully");
+                                  }
+
+                    });
+            }
+    });
+
+});
+
+module.exports = router;
+
